dbbceffcc71703c770da05bb3cb1fadc97e5c7c1
quentingonus <55360738+quentingonus@users.noreply.github.com>
2021-01-24 01:06:40 +0630

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
